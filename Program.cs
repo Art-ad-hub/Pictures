9eb0fe025bf704d496d95eaaa295c936f4980ede
Artem <130403999+Art-ad-hub@users.noreply.github.com>
2023-04-12 00:28:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pictures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pictureLine = 3;
+            int pictureTotal = 52;
+            int rowsFilled = pictureTotal / pictureLine;
+            int pictureRemains = pictureTotal % pictureLine;
+
+            Console.WriteLine($"Полностью заполненных рядов {rowsFilled}, а картинок сверх меры {pictureRemains}.");
+        }
+    }
+}
